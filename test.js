9c348cde03fd0e32d808e957018b3c5ad75c3137
Vyacheslav <slava-russia@mail.ru>
2023-01-24 14:45:22 +0700

    Program written  in test.js

--- test.js
+++ test.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let arrayInitial = ['Russia', 'Denmark', 'UAE', 'Germany', 'USA', 'UK'],
+    arrayResulted = [];
+
+for (let i = 0; i < arrayInitial.length; i++) {
+    if (arrayInitial[i].length <= 3) {
+        arrayResulted.push(arrayInitial[i]);
+    }
+}
+
+console.log(arrayResulted);
